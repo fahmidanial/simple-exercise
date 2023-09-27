df74db8c5c67bb03f515436de4c06ecd827c4ff6
fahmidanial <naloniel2000@gmail.com>
2023-09-27 16:06:30 +0800

    self defined method

---
+++ self-defined-method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Display();
+
+    }
+    static void Display()
+    {
+        Console.WriteLine("Welcome to C# Programming");
+        Console.WriteLine("I hope you enjoy the class");
+    }
+}
+
+
+
+
