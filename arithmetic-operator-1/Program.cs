c2a2bd379654538f77b3a4ab2dcd4944f2182e8e
fahmidanial <naloniel2000@gmail.com>
2023-09-27 14:20:33 +0800

    arithmetic-operator-example

---
+++ arithmetic-operator-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n1, n2;
+        //int answer;
+        double answer;
+        Console.WriteLine("Enter value 1: ");
+        n1 = int.Parse(Console.ReadLine());  //5
+
+        Console.WriteLine("Enter value 2: ");
+        n2 = int.Parse(Console.ReadLine()); //2
+
+        answer = (double)n1 / n2;
+
+        Console.WriteLine("The division outcome: {0}", answer);
+
+
+    }
+}
