758dc922b39f8bbd7ca1c71789cf2d1e269b7e23
fahmidanial <naloniel2000@gmail.com>
2023-09-27 15:09:45 +0800

    boolean

---
+++ boolean/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool yes = true, no = false;
+
+        Console.Write("AND Logic:");
+        Console.Write("\t (yes && yes) : " + (yes && yes));
+        Console.Write("\n\t\t (yes && no ) : " + (yes && no));
+        Console.Write("\n\t\t (no  && no ) : " + (no && no));
+
+        Console.Write("\n\nOR Logic:");
+        Console.Write("\t (yes || yes) : " + (yes || yes));
+        Console.Write("\n\t\t (yes || no ) : " + (yes || no));
+        Console.Write("\n\t\t (no  || no ) : " + (no || no));
+
+        Console.Write("\n\nNOT Logic:");
+        Console.Write("\t yes = " + yes);
+        Console.Write("\t !yes = " + !yes);
+        Console.Write("\n\t\t no  = " + no);
+        Console.Write("\t !no  = " + !no);
+
+
+
+
+    }
+}
