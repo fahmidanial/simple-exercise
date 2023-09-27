c0e95498005403ef935178c1077cf06cfd51295c
fahmidanial <naloniel2000@gmail.com>
2023-09-27 16:16:26 +0800

    self defined method 1

---
+++ self-defined-method-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Display();
+
+        Power2(10);
+
+    }
+    static void Display()
+    {
+        Console.WriteLine("Welcome to C# Programming");
+        Console.WriteLine("I hope you enjoy the class");
+    }
+
+    static void Power2(int n1)
+    {
+        int answer = n1 * n1;
+        Console.WriteLine($"The power of 2 value: {answer}");
+    }
+}
+
+
+
+
+
+
+
