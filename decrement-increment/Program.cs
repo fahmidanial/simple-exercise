1c441f9b114933887c142bb8bad7f2c2dd359c14
fahmidanial <naloniel2000@gmail.com>
2023-09-27 15:02:24 +0800

    -19

---
+++ decrement-increment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int f1 = 70, f2 = 90, total;
+
+        total = f1++ - --f2;
+
+        Console.WriteLine($"total : {total}");
+
+
+
+    }
+}
