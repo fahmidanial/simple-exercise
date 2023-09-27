2f9951e9d50503f82b8058e78e52fab61dd6c175
fahmidanial <naloniel2000@gmail.com>
2023-09-27 14:56:15 +0800

    16

---
+++ increment-1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n1 = 5, n2 = 20;
+        int answer;
+        answer = ++n2 - n1;
+        Console.WriteLine($"answer : {answer}");
+
+    }
+}
---
+++ increment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int y = 10;
+        Console.WriteLine($"Before increment y: {y}");
+        y = y + 1;
+        Console.WriteLine($"After increment y: {y}");
+    }
+}
