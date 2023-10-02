5c87de1722613f04c1a11969e2529cf88f31664d
fahmidanial <naloniel2000@gmail.com>
2023-10-02 15:00:08 +0800

    multi array

---
+++ array-pass-by-ref/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+            
+            int[] arr = new int[] { 2, 5, 8, 9, 10, 6, 9 };
+            Power2(arr); //passing by reference or by value ? --> if whole array then it is by reference
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+        }
+        static void Power2 (int [] arrV)
+        {
+            for(int i = 0; i < arrV.Length; i++)
+            {
+                arrV[i] = arrV[i] * arrV[i];
+             }
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ multiarray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[,] marr;
+        marr = new int[2, 3];
+
+        for (int r = 0; r < 2; r++)
+        {
+            for (int c = 0; c < 3; c++)
+            {
+                Console.WriteLine("Enter the value for multi dimensional array:");
+                marr[r, c] = int.Parse(Console.ReadLine());
+            }
+        }
+
+
+        for (int r = 0; r < marr.GetLength(0); r++)
+            for (int c = 0; c < marr.GetLength(1); c++)
+                Console.WriteLine(marr[r, c]);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
