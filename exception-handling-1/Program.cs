da5969862ca1ae4cd6ad07f2bcf4dba7aa93dc01
fahmidanial <naloniel2000@gmail.com>
2023-10-03 17:02:14 +0800

    last

--- exception-handling-1/Program.cs
+++ exception-handling-1/Program.cs
@@ -35,6 +35,8 @@ class Program
         } while (!flag);
     }
 
+
+
 }
 
 
---
+++ exception-handling-2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        try
+        {
+            int n;
+            int[] arr = new int[] { 10, 20, 30, 40, 50 };
+            Console.WriteLine("Enter which index of the array you want to display: ");
+            n = int.Parse(Console.ReadLine()); //formatexception aa
+
+            Console.WriteLine(arr[n]);
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Bye bye");
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
